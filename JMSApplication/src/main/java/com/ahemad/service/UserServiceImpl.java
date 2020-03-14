a8b1b1559ec98ff60a6b1f7fb64a53e216debcc5
zmq185 <juber.ahemad@yahoo.com>
2020-03-14 15:59:56 +0530

    zxcd

--- JMSApplication/src/main/java/com/ahemad/service/UserServiceImpl.java
+++ JMSApplication/src/main/java/com/ahemad/service/UserServiceImpl.java
@@ -1,5 +1,12 @@
 package com.ahemad.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ahemad.dao.UserDao;
+
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	UserDao userDao;
 
 }
