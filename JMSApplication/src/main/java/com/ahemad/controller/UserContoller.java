ae7bc4da9f5fc1d81a0c561a212b350f68be5bf4
zmq185 <juber.ahemad@yahoo.com>
2020-03-16 16:40:06 +0530

    dcsd

--- JMSApplication/src/main/java/com/ahemad/controller/UserContoller.java
+++ JMSApplication/src/main/java/com/ahemad/controller/UserContoller.java
@@ -3,6 +3,7 @@ package com.ahemad.controller;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ahemad.dao.DBConnection;
+import com.ahemad.entity.User;
 import com.ahemad.service.UserService;
 import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
 
@@ -43,11 +45,16 @@ public class UserContoller {
 	public ModelAndView myConnection() {
 
 		String sql="select * from user";
+		ArrayList<User> list = new ArrayList<User>();
 		try{
 			Connection connection = DBConnection.getConnction();
 			PreparedStatement ps = connection.prepareStatement(sql);
 			ResultSet resultSet=ps.executeQuery();
 			System.out.println("All User :: "+resultSet);
+			while(resultSet.next()) {
+				System.out.println("All User :: "+resultSet.getString("name"));
+				
+			}
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
